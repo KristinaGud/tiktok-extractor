43a8438b7d89b18142b38cd110dd955a2f4c4fea
Kristina <gudaityte.kristina@gmail.com>
2019-05-24 15:51:26 +0200

    refactoring

--- src/main/java/tiktok/App.java
+++ src/main/java/tiktok/App.java
@@ -1,34 +1,48 @@
 package tiktok;
 
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import result.AppResult;
 import result.CommentDataResult;
 import result.VideoDataResult;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class App {
+    static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    static final Gson GSON = new Gson();
+
     public static void main(String[] args) throws InterruptedException, IOException {
+        App app = new App();
+        AppResult VideosResult = app.showResults(args[0]);
+        LOGGER.info(parseResultToJson(VideosResult));
+    }
+
+    public AppResult showResults(String keyword) throws InterruptedException, IOException {
         Collector collector = new Collector();
         Extractor extractor = new Extractor();
         UrlGenerator urlGenerator = new UrlGenerator();
 
-        VideoDataResult ladyGagaVideos = collector.collectVideoData("ladygaga");
-        Set <String> extractIds = extractor.extractIds(ladyGagaVideos.getVideoData());
+        VideoDataResult analysedVideos = collector.collectVideoData(keyword);
+        Integer numFoundVideos = analysedVideos.getVideoData().size();
+        Set <String> extractIds = extractor.extractIds(analysedVideos.getVideoData());
         List <String> urlsToFirstPageComments = urlGenerator.generateUrlsToFirstCommentsPage(extractIds);
-        List <String> allLadyGagaTags = extractor.extractHashTags(ladyGagaVideos.getMessages());
-
-        System.out.println("Num of unique tags used in video descriptions: " + extractor.countUniqueTags(allLadyGagaTags));
-        System.out.println("Top 10 tags in videos descriptions: " + extractor.pickOutTopHashTags(allLadyGagaTags, 10));
-
+        List <String> allTags = extractor.extractHashTags(analysedVideos.getMessages());
+        long uniqueTags = extractor.countUniqueTags(allTags);
+        Map <String, Long> topTags = extractor.pickOutTopHashTags(allTags, 10);
         List <String> urlResponses = collector.collectUrlResponses(urlsToFirstPageComments);
-
         CommentDataResult commentDataResult = extractor.extractCommentDataResults(urlResponses);
         List <String> commentsTextMessages = commentDataResult.getTextMessages();
         List<String> dates = commentDataResult.getDates();
 
-        System.out.println("List of internet users comments related to analyzed videos: " + commentsTextMessages);
-        System.out.printf("All comments were posted from %s till %s", extractor.getStartDate(dates), extractor.getEndDate(dates));
+        return new AppResult(extractor.getStartDate(dates), extractor.getEndDate(dates), numFoundVideos, topTags, uniqueTags, commentsTextMessages);
+    }
 
+    public static String parseResultToJson(AppResult appResult) {
+        return GSON.toJson(appResult);
     }
 }
